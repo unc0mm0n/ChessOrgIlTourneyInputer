902373130829006443a1f96070e8f8c597d2c98c
Yuval Wyborski <yvw.bor@gmail.com>
2015-09-04 17:13:10 +0300

    First actually working version..

--- chessil_tourney_inserter/command_line.py
+++ chessil_tourney_inserter/command_line.py
@@ -1,10 +1,7 @@
-import tourney_inserter as cti
+import chessil_tourney_inserter.chessil_tourney_inserter as cti
 import sys
 
 def main():
-    if len(sys.argv) == 1:
-        print("Usage: chessil_tourney_inserter.py *tournament name*")
-        exit()
     cti.main();
     
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup
 
 setup(name='chessil_tourney_inserter',
-      version='1.1',
+      version='1.2.3',
       description='Uploads tournament data to Israel\'s chess federation site from SwissPerfect 98 generated SPDE files',
-      url='http://github.com/unc0mm0n/ChessOrgIlTourneyInputer',
+      url='http://github.com/unc0mm0n/chessil_tourney_inserter',
       author='Yuval Wyborski',
       author_email='yvw.bor@gmail.com',
       packages=['chessil_tourney_inserter'],
       zip_safe=False,
+
+      install_requires = [
+    'selenium'
+    ],
+
       entry_points={
       'console_scripts': [
-            'insertchessiltourney = chessil_tourney_inserter.command_line:main']
+            'insertchessiltournament = chessil_tourney_inserter.command_line:main']
       })
